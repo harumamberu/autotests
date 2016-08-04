b2a63ee61aa6736e48b5368f38c78431f409ce7d
harumamberu <dmytrenko.mykhaylo@gmail.com>
2016-08-04 21:24:05 +0300

    Lection10

---
+++ src/com/harumamberu/Palindrome.java
@@ -0,0 +1,30 @@
+package com.harumamberu;
+
+import java.util.Scanner;
+
+/**
+ * Created by Мишка on 25.07.16.
+ */
+public class Palindrome {
+    public static void main(String[] args) {
+        int l;
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        char[] ch = word.toCharArray();
+        l = word.length();
+        int flag = 0;
+        for (int i = 0; i < l/2; i++){
+            if (ch[i]==ch[l - i - 1]) {
+                System.out.println(ch[i] + " = " + ch[l - i - 1]);
+                flag++;
+            }else{
+                flag = 0;
+                System.out.println("not palindrome");
+            }
+            if (flag == l/2){
+                System.out.println("Is palindrome");
+            }
+        }
+
+    }
+}

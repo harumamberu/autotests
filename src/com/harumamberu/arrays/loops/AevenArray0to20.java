f315b79e2fe9417a12e11765b0800185b23d60cd
harumamberu <dmytrenko.mykhaylo@gmail.com>
2016-08-27 11:21:06 +0300

    refactor arrayprint etc

--- src/com/harumamberu/arrays/loops/AevenArray0to20.java
+++ src/com/harumamberu/arrays/loops/AevenArray0to20.java
@@ -6,7 +6,7 @@ public class AevenArray0to20 {
     public void evenArray0to20() {
         int[][] evenArray = new int[1][10];
         int k = 2;
-        for ( int i = 0; i < evenArray.length; i++){
+        for ( int i = 0; i < evenArray[0].length; i++){
             evenArray[0][i] = k;
             k+=2;
         }
